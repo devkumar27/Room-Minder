27bc963003596e4cda6998a30c71559692ac50b3
devkumar27 <dev270502@gmail.com>
2024-02-10 23:01:39 +0530

    build: home page design

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import './styles/home.css';
+import { useCookies } from 'react-cookie';
 
 const Home = () => {
-  return (
-    <div>
+  const [cookies] = useCookies(['token']);
+  const navigate = useNavigate();
 
-        <h1>Welcome to RoomMinder</h1>
+  useEffect(() => {
+    if(cookies.token) {
+      navigate('/api/complaints')
+    }
+  }, [])
+
+  return (
+    <div className='box'>
+
+        <div className='box-element'>
+            <h1 className='page-heading heading-1'>Welcome to RoomMinder</h1>
+            <p>Your one stop for Hostel Room Maintenance</p>
+            <button className='btn'><a><Link to="api/user/login">Login</Link></a></button>          
+            <button className='btn'><Link to="api/user/">Register</Link></button> 
+        </div>
 
     </div>
   )
